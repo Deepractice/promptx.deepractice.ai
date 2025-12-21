f6c4ac0ea8d1bb85a39df41c2ae316c9bc04dc00
Carson <ax6101@gmail.com>
2025-12-21 23:19:55 +0800

    fix: correct Windows icon SVG path
    Replace distorted Windows icon with official Simple Icons Windows 10 path.
    The previous path had incorrect coordinates causing visual distortion.

--- components/OSIcons.tsx
+++ components/OSIcons.tsx
@@ -19,7 +19,7 @@ export function WindowsIcon({ className = "w-12 h-12" }: { className?: string })
       fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path d="M3 5.45v6.11l8.02.09V3.4L3 5.45zm8.02 6.88L3 12.4v6.09l8.02 2.07v-8.23zm.93-9.15l9.05-1.28v8.2l-9.05.06V3.18zm9.05 8.98l-.05 8.25-9 1.28v-8.38l9.05-.15z" />
+      <path d="M0 3.449L9.75 2.1v9.451H0m10.949-9.602L24 0v11.4H10.949M0 12.6h9.75v9.451L0 20.699M10.949 12.6H24V24l-12.9-1.801" />
     </svg>
   );
 }
